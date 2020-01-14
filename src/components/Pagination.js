3914abf0786698f7b00817599bdf8628eb9c9b8e
Jean-Paul Tres <jp.tres@gmail.com>
2020-01-14 17:35:08 -0500

    [MFT-4] jtres React components

---
+++ src/components/Pagination.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TablePagination from "@material-ui/core/TablePagination";
+
+const Pagination = ({ ...props }) => (
+  <div className="pagination">
+    <TablePagination {...props}/>
+  </div>
+)
+
+export default Pagination
